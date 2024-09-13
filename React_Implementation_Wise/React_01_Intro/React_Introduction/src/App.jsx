7f22dac7cdb3dd7af970022ff2b58ad078c1aa99
sachindevtech <sachin.1107sk@gmail.com>
2024-09-14 01:03:04 +0530

    Intro to React is added

---
+++ React_Implementation_Wise/React_01_Intro/React_Introduction/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <>
+    <Navbar/>
+     <div className = "hello">Hellow Ji, Today i started learning React!</div>
+    </>
+  )
+}
+
+export default App
---
+++ React_Implementation_Wise/React_01_Intro/React_Introduction/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+const navbar = () => {
+  return (
+  <>
+
+    <div className='navbar'>
+      <div>Home</div>
+      <div>Courses</div>
+      <div>Login</div>
+      <div>Signup</div>
+    </div>
+
+  </>
+  )
+}
+
+export default navbar
